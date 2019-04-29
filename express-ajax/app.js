d4321e0b215b8f6c4c01959e0811c0ea8a4029d4
Peter Pae <paendabear@gmail.com>
2019-04-29 15:36:57 -0400

    add part 2

--- express-ajax/app.js
+++ express-ajax/app.js
@@ -17,4 +17,21 @@ app.get('/sum', (request, response) => {
   response.status(200).json(output)
 });
 
+app.get('/cipher', (request, response) => {
+  const query = request.query;
+  const text = query.text;
+  const shift = Number(query.shift);
+
+
+
+  let charArr = text.toLowerCase().split('');
+  let numArr= [];
+  charArr.forEach (char => numArr.push(String.fromCharCode(((char.charCodeAt(0)-97+shift)%26)+97)));
+
+
+  const output = `${numArr.join('')}`;
+
+  response.status(200).json(output)
+});
+
 app.listen(8080, function() {console.info(`Server is listening on ${this.address().port}`);});
